802f56cfee8f944101e32db2cc88e8e2fdad7339
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-04-21 16:44:14 +0530

    amazon product scrape & bright data API

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const Product = require('./models/product.model');
 const connectToDB = require('./mongoose');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require('dotenv').config();
+const {isValidAmazonProductURL} = require('./lib/utils/valid_url_check');
+const {scrapeAmazonProduct} = require('./lib/scraper');
 
 const app = express();
 app.use(cors());
@@ -49,6 +51,22 @@ app.get('/products/similar/product/:id', async (req, res) => {
         res.json(similarproduct);
     } catch (error) {
         res.status(500).send({ message: "An error occurred while fetching the product details", error: error.message });
+    }
+});
+
+// Insert amazon product
+app.post('/products/amazon', async (req, res) => {
+    try {
+        console.log("Hello");
+        const {amazonurl} = req.body;
+        const validURL = isValidAmazonProductURL(amazonurl);
+        if(!validURL){
+            res.status(404).json({message: "Enter a valid amazon URL"});
+        }
+        const scrapeamazondata = await scrapeAmazonProduct(amazonurl);
+        console.log(scrapeamazondata);
+    } catch (error) {
+        res.status(500).send({ message: "An error occurred while getting the product details", error: error.message });
     }
 });
 
