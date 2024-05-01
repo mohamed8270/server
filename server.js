67094eb3c098c5e957d6d8371f7d65922a29ab64
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-05-01 11:40:53 +0530

    body parser checking

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const { scrapeAndStoreProduct } = require('./lib/actions');
 const app = express();
 app.use(cors());
 app.use(express.json());
+// app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 const port  = process.env.PORT || 3000;
 const localhostUrl = `http://localhost:${port}`;
